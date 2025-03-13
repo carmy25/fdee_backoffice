3ea9d45b0a9f1af794f81e131c6844bc67a9b1a7
Mykola Kharechko <crchemist@gmail.com>
2025-03-13 11:34:40 +0200

    add tests for place app

--- place/tests.py
+++ place/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from functools import partial
+from django.contrib.auth.models import User
+from rest_framework.test import APIRequestFactory, APITestCase
 
-# Create your tests here.
+
+class PlaceTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test', first_name='test', last_name='test', email='test@gmail.com', password='Test1234')
+        self.client.force_login(self.user)
+
+    def test_create_place(self):
+        response = self.client.post(
+            '/api/places/',
+            data={'name': 'Test place'},
+            format='json'
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['name'], 'Test place')
