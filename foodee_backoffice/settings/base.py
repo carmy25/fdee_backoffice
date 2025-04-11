71811e4ce39a706d18e964ae90520d4f08c14bdb
Mykola Kharechko <crchemist@gmail.com>
2025-04-11 13:13:29 +0300

    updated settings

--- foodee_backoffice/settings/base.py
+++ foodee_backoffice/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+ROOT_URLCONF = 'foodee_backoffice.urls'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 FIXTURE_DIRS = [BASE_DIR / "fixtures"]
