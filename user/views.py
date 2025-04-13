03e14c50cb02ac02a36dd8bcaebbd2459bc11c5d
Mykola Kharechko <crchemist@gmail.com>
2025-04-13 11:57:35 +0300

    return 401 on incorrect username/password

--- user/views.py
+++ user/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import redirect, render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        try:
+            serializer.is_valid(raise_exception=True)
+        except Exception as e:
+            return Response(
+                {'error': 'invalid username or password'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
